08665a80660b14b5fed8abb2d3e2d787ea195ba9
zhanghu <zhang@qq.com>
2019-08-29 18:12:05 +0800

    第一次提交到的git

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("===随便来点东西===");
+        System.out.println("===随便来点东西====");
     }
 }
